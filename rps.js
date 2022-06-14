c6d9d05882eae798ed90c3ac3988c60dca9b4373
The_Baum123 <the.baum12345@gmail.com>
2022-06-14 09:09:39 +0200

    i dont even know

---
+++ rps.js
@@ -0,0 +1,14 @@
+let player_choice = ""
+
+function chose_rock(){
+    let player_choice = 1
+    console.log(player_choice)
+}
+
+function chose_paper(){
+    let player_choice = 2
+}
+
+function chose_scissors(){
+    let player_choice = 3
+}
